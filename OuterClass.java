a349527252bf59d9fa9065e65206a53ae991a392
Rahul <vlakshetti@gmail.com>
2018-11-02 07:35:13 +0530

    First Commit

---
+++ OuterClass.java
@@ -0,0 +1,28 @@
+//package com.test.java;
+//
+//public class OuterClass{
+//	
+//	
+//	private int outerclassVariable = 10;
+//	
+//	
+//	public  void vmethodForInnerClass() {
+//		
+//		 class MethodInnerclass {
+//			
+//			 public void methodFoInnerclass() {
+//				 
+//				 System.out.println(outerclassVariable);
+//			 }
+//			
+//		}
+//		 MethodInnerclass m = new MethodInnerclass();
+//		m.methodFoInnerclass();
+//		
+//	}
+//	
+//	
+//	
+//	
+//	
+//}
